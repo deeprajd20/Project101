02d21cd077b9b22e4660cd178852b30ec454d3e1
deepraj <deeprajd71@gmail.com>
2024-11-24 23:26:22 +0530

    databse connection error

--- model/user_model.py
+++ model/user_model.py
@@ -2,20 +2,20 @@ import mysql.connector
 from mysql.connector import Error
 
 class user_model:
-    def __init__(self):
-        try:
-            temp_data = {
-                "host": "127.0.0.1",
-                "port": 3306,
-                "user": "root",
-                "password": "root"
-            }
-            connection = mysql.connector.connect(**temp_data)
-            if connection.is_connected():
-                print("Successfully connected to the database")
-        except Error as e:
-            print(f"Error connecting to the database: {e}")
-            connection = None
+    # def __init__(self):
+    #     try:
+    #         temp_data = {
+    #             "host": "127.0.0.1",
+    #             "port": 3306,
+    #             "user": "root",
+    #             "password": "root"
+    #         }
+    #         connection = mysql.connector.connect(**temp_data)
+    #         if connection.is_connected():
+    #             print("Successfully connected to the database")
+    #     except Error as e:
+    #         print(f"Error connecting to the database: {e}")
+    #         connection = None
         
 
     def user_model_test(self):
